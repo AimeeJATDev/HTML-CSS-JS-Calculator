b13a98646d400f94d0a177e904654b4c449f5ecc
Aimee Taylor <AimeeJATDeveloper@outlook.com>
2023-11-24 13:01:26 +0000

    Made a start on JS code

---
+++ calculator.js
@@ -0,0 +1,14 @@
+
+let inputVal;
+
+function clear() {
+    document.getElementById("numInput").value = "";
+}
+
+function numOne() {
+    document.getElementById("numInput").value = "1";
+}
+
+function numTwo() {
+    document.getElementById("numInput").value = "2";
+}
