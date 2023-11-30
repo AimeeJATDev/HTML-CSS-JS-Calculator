c1834efb35f7f23acd8e894aaf66eaee311bc1d5
Aimee Taylor <AimeeJATDeveloper@outlook.com>
2023-11-30 13:21:45 +0000

    Created a function to input numbers and removed functions that are no longer required

--- calculator.js
+++ calculator.js
@@ -5,14 +5,6 @@ function clr() {
     document.getElementById("numInput").value = "";
 }
 
-function numOne() {
-    document.getElementById("numInput").value += "1";
-}
-
-function numTwo() {
-    document.getElementById("numInput").value += "2";
-}
-
-function enterNum() {
-
+function enterNum(btnId) {
+    document.getElementById("numInput").value += btnId;
 }
