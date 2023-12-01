78188f26559cb1bd486e8228bd1b0315ab283250
Aimee Taylor <AimeeJATDeveloper@outlook.com>
2023-12-01 13:06:10 +0000

    Added enterNum function to all number buttons and added a delete function

--- calculator.js
+++ calculator.js
@@ -5,6 +5,12 @@ function clr() {
     document.getElementById("numInput").value = "";
 }
 
+function del() {
+    let oldVal = document.getElementById("numInput").value;
+    let newVal = oldVal.slice(0, -1);
+    document.getElementById("numInput").value = newVal;
+}
+
 function enterNum(btnId) {
     document.getElementById("numInput").value += btnId;
 }
