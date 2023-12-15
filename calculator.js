bf4d21c256798c2f35ff47aedd930095c815c7bb
Aimee Taylor <AimeeJATDeveloper@outlook.com>
2023-12-15 13:05:19 +0000

    Added comments to JS code

--- calculator.js
+++ calculator.js
@@ -1,23 +1,26 @@
 
-let inputVal;
+//let inputVal;
 
 function clr() {
+    // Clears everything from the input
     document.getElementById("numInput").value = "";
 }
 
 function del() {
     let oldVal = document.getElementById("numInput").value;
+    // Deletes the last character in the input
     let newVal = oldVal.slice(0, -1);
     document.getElementById("numInput").value = newVal;
 }
 
 function enterNum(btnId) {
+    // Adds the id of button to the input
     document.getElementById("numInput").value += btnId;
 }
 
 function decimal() {
     let input = document.getElementById("numInput").value;
-    // Checks whether there is already a decimal point
+    // Checks whether the input includes a decimal point
     const regex = new RegExp ("\\.", "g");
     let search = regex.test(input);
 
@@ -30,9 +33,11 @@ function decimal() {
 function symbols(btnId) {
     let input = document.getElementById("numInput").value;
     
+    // Checks whether the input includes a maths symbol
     const regex = new RegExp("[^0-9.]", "g");
     let search = regex.test(input);
 
+    // If no and input isn't blank, the maths symbol will be added
     if (search == false && input != "") {
         document.getElementById("numInput").value += btnId;
     }
@@ -41,16 +46,20 @@ function symbols(btnId) {
 function pos_neg() {
     let input = document.getElementById("numInput").value;
 
+    // Checks whether number has a minus at the start
     if (input[0] == "-") {
+        // If it does, remove the minus
         let newVal = input.substring(1);
         document.getElementById("numInput").value = newVal;
     }
     else {
+        // If not, add a minus
         let newVal = "-" + input;
         document.getElementById("numInput").value = newVal;
     }
 }
 
 function equals() {
-
+    let firstNum;
+    let secondNum;
 }
